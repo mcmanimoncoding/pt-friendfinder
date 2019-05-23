958b6f8b67b5658383bf4c0b5b806351fe92ef0a
Daniel M <mcmanimoncoding@gmail.com>
2019-05-22 17:04:15 -0700

    update

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,14 +6,44 @@ module.exports = function (app){
     });
 
     app.post("/api/friends", function(req,res){
-        let friendMatch = {
+        console.log(req.body)
+
+        //create variable for inserting best match
+        let user = req.body;
+        let userScore = user.scores;
+        let relativeFriendScore;
+        let scoreDiff = 0;
+        let bestFriend = {
             name: "",
             photo: "",
-            
+            difference: ""
         }
+
+        console.log(userScore);
+
+        
+        // friends.forEach(function, (friend, i))
+        for (var i = 0; i<friends.length; i++){
+            console.log(friends[i]);
+            let relativeFriendScore = 0;
+            
+            for (var x = 0; x<friends[i].scores[x]; x++){
+                relativeFriendScore += Math.abs(parseInt(userScore[x]) - parseInt(friends[i].scores[x]));
+                
+                if (relativeFriendScore <= bestFriend.difference){
+                    bestFriend.name = friends[i].name;
+                    bestFriend.photo = friends[i].photo;
+                    bestFriend.difference = relativeFriendScore;
+                }
+            }
+
+        }
+
     })
 
+    friends.push(user);
+    res.json(bestFriend);
 
-
+    
 }
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -13,4 +13,8 @@ module.exports = function(app){
     app.use(function(req,res){
         res.sendFile(path.join(__dirname + "/../public/home.html"))
     });
+
+
+    
+    
 }
